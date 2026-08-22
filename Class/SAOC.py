086c77a78d294893638bf642568cdcd378914869
Nekoiio <dguille2023@gmail.com>
2026-08-22 16:22:09 -0400

    Finalized class SAOC|decrypt/encrypt & tested

--- Class/SAOC.py
+++ Class/SAOC.py
@@ -1,7 +1,7 @@
 from Crypto.Cipher import AES
 
 
-class SAOFB:
+class SAOC:
     _key: bytearray = bytes.fromhex(
         "41 33 66 7A 67 39 38 75 30 63 50 4F 34 64 67 72"
         "6E 6A 45 30 6F 37 38 69 65 34 33 67 37 35 77 32"
@@ -10,9 +10,10 @@ class SAOFB:
     )
     _AESKEY = _key[:32]
     _CRYPT = AES.new(_AESKEY, AES.MODE_ECB)
+    SAVE_LOC_FILE: str =""
 
     def decryptSave(self, inputFile: str, outputFile: str) -> bool:
-        with open(inputFile, 'rb') as f:
+        with open(self.SAVE_LOC_FILE + inputFile, 'rb') as f:
             data: bytearray = f.read()
 
         out: bytearray = bytearray()
@@ -25,14 +26,24 @@ class SAOFB:
 
             out += self._CRYPT.decrypt(block)
 
-        with open(outputFile, "wb") as f2:
+        with open(self.SAVE_LOC_FILE + outputFile, "wb") as f2:
             f2.write(out)
 
         return True
 
     def encryptSave(self, inputFile: str, outputFile: str) -> bool:
-        with open(inputFile, "rb") as f:
+        with open(self.SAVE_LOC_FILE + inputFile, "rb") as f:
             data: bytearray = f.read()
 
         out: bytearray = bytearray()
         for j in range(0, len(data), 16):
+            block: bytearray = block[j:j+16]
+            if (len(block) < 16):
+                out += block
+                break
+            out += self._CRYPT.encrypt(block)
+
+        with open(self.SAVE_LOC_FILE + outputFile, "wb")as f2:
+            f2.write(out)
+
+        return True
---
+++ main.py
@@ -0,0 +1,10 @@
+from Class.SAOC import SAOC 
+
+SAOC.SAVE_LOC_FILE = "SAOFBS/"
+
+def main():
+    SAOC.decryptSave(SAOC, "SaveData.sav", "Decryted.sav")
+
+
+if __name__ == "__main__":
+    main()
